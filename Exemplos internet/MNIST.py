a7b0eb4cd34c0e44a7c296cffbb293781096374f
DaniloClementeCoelho <daniloclementecoelho@gmail.com>
2021-01-12 11:47:47 -0300

    inicio MNIST

---
+++ Exemplos internet/MNIST.py
@@ -0,0 +1,13 @@
+import gzip
+import struct
+import numpy as np
+
+def read_idx(filename):
+    with gzip.open(filename) as f:
+        zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
+
+
+
+raw_train = read_idx("./Bases/MNIST/train-images-idx3-ubyte.gz")
